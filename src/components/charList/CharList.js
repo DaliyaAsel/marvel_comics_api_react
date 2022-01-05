72523ef9178e20af6813aac0a4794d7d58752443
DaliyaAsel <theblackdahlia@mail.ru>
2022-01-05 20:26:31 +0700

    added style, focus, ref

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -61,20 +61,46 @@ class CharList extends Component {
         })
     }
 
+    // работа с рефами
+    itemRefs = [];
+    setActiveRef = (elem) => {
+        this.itemRefs.push(elem);
+    }
+
+    // работа с рефами и добавление класса к ним
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        // this.itemRefs[id].focus();
+
+    }
+
     // Этот метод создан для оптимизации, 
     // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
             }
+
             
             return (
-                <li 
+                <li ref = { this.setActiveRef }
                     className="char__item"
                     key={item.id}
-                    onClick={()=> this.props.onCharSelected(item.id)}   >
+                    onClick={()=> { this.props.onCharSelected(item.id);
+                                    this.focusOnItem(i);
+                    }} 
+                    
+                   /* onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}*/
+
+                >
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
